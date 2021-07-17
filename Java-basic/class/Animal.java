3f519704112ef8e9d612f0ab0cf5c014dde7fd2f
StevanusAndika <84663886+StevanusAndika@users.noreply.github.com>
2021-07-17 14:02:24 +0700

    Create Animal.java

---
+++ Java-basic/class/Animal.java
@@ -0,0 +1,22 @@
+public class Animal{
+   
+   
+  
+  String jenis;
+  String warna;
+  
+  void bersuara(){
+    
+  }
+  void makanan()
+  {
+    
+  }
+  void aktivitas()
+  {
+    
+  }
+  public static void main(String [] args){
+    System.out.println("Contoh class");
+  }
+}
