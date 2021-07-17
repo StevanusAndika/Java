d09fe8950229f3881a59b9fdeffa525b059a69cd
StevanusAndika <84663886+StevanusAndika@users.noreply.github.com>
2021-07-17 14:15:23 +0700

    add gaji_pegawai.java di folder variabel/lokal

---
+++ Java-basic/Variabel/Lokal/gaji_pegawai.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class gaji_pegawai
+ {
+     public void gajipegawai()
+     {
+       double gaji=0;
+       gaji=gaji+100;
+       
+       System.out.println("Jumlah gaji adalah:"+gaji);
+       int hari=0;
+       hari=hari+25;
+       System.out.println("Lama bekerja(hari):"+hari);
+       
+       
+     }
+     
+   public static void main(String args[])
+   { 
+     gaji_pegawai coba=new gaji_pegawai();
+     coba.gajipegawai();
+    
+   }
+ }
