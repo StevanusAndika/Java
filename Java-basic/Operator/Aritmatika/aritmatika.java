9b5d6317468234e95e3d4183e5795d7123de6f95
StevanusAndika <84663886+StevanusAndika@users.noreply.github.com>
2021-07-17 14:27:20 +0700

    Create aritmatika.java

---
+++ Java-basic/Operator/Aritmatika/aritmatika.java
@@ -0,0 +1,38 @@
+import java.util.*;
+ // Compiler version JDK 11.0.2
+
+ class Aritmatika
+ {
+   public static void main(String args[])
+   { 
+     int X=100;
+     int Y=110;
+    System.out.println("X:"+(X));
+    System.out.println("Y:"+(Y));
+    
+    //operator penjumlahan:menambahkan operand
+     System.out.println("X+Y:"+ (X+Y));
+    //operator pengurangan:mengurangin operand
+    
+     System.out.println("X-Y:"+(X-Y));
+     //operator pengurangan:mengurangi operand
+     
+     System.out.println("X/Y:"+(X/Y));
+     //operator pembagian:berfungsi membagi operand
+     
+     //operator modulus:mengurangkan nilai operand disebelah kiri dgn nilai operand  disebelah kanan dan mengembalikan sisa nilai.
+     System.out.println("X%Y:"+(X%Y));
+    
+    
+    
+       
+     //penurunan:mengurangkan satu nilai operand
+     System.out.println("X:"+(--X));
+     
+     
+    //peningkatan:menambahkan satu nilai operand
+    
+   System.out.println("Y:"+(++Y));
+     
+   }
+ }
