75fe1fc41c766adabfdcd6f736b7258a47810633
StevanusAndika <84663886+StevanusAndika@users.noreply.github.com>
2021-07-17 14:29:18 +0700

    Add files via upload

---
+++ Java-basic/Operator/relasional.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class  relasional
+ {
+   public static void main(String args[])
+   { 
+     int A=50;
+     int B=60;
+     //==:sama dengan
+     System.out.println("A:"+(A));
+     System.out.println("B:"+(B));
+     System.out.println("A==B:"+(A == B));
+     //!==:Tidak sama dengan
+     
+     System.out.println("A!=B:"+(A != B));
+     
+     System.out.println("A<B:"+(A <B));
+      //<:Kurang
+      
+       //>:Lebih
+     System.out.println("A>B:"+(A >B));
+//<=:kurang dari
+
+//>=:lebih dari
+     System.out.println("A<=B:"+(A<= B));
+     System.out.println("A>=B:"+(A>=B));
+     
+     
+     
+     
+   }
+ }
