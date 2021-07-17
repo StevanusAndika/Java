992629a576e2032fc1b4e531f4c1c93b4aea70b6
StevanusAndika <84663886+StevanusAndika@users.noreply.github.com>
2021-07-17 14:00:06 +0700

    Create dasar.java

---
+++ Java-basic/dasar.java
@@ -0,0 +1,41 @@
+*Aturan penulisan java:
+   -Case sensitive:Hello berbeda dgn hello
+   -Aturan penulisan nama class:bids membuat class dgn nama Apapun
+   penulisan nama class,harus diawali/menggunakan huruf kapital
+   
+   -Penamaan method:method  harus diawalin dgn huruf kecil:
+     contoh:public void mymetode()
+     -penamaan file:nama file harus sama dgn nama class
+     
+     public static void main(String args[]):
+       merupakan  pemrosesan program Java,merupakan bagian yg harus ada dalam setiap program java
+       
+       identifier java:
+         -semua identifier harus diawalin dgn  huruf(A-Z atau a-z,karakter $,atau karakter_(underscore))
+         
+         -Setelah kata pertama,identifier dapat brrisi kombinasi karakter apa saja
+         
+         -sebuah kata kunci (reserved word)tidak dapat digunakan dlm identifier
+         
+         -identifier bersifat case sensitive
+         
+         
+         modifier Java:
+           dapat memofifikasi class,method,dan sebagainya dhn menggunakan modifier
+           
+           dua katergori modifier dlm java:
+            - modifier akses:default,public,protected,private
+            -modifier Non akses:final,abstract,strictftp.
+            
+            penulisan komentar:
+              //:satu baris
+             /*:lebih dari satu baris
+               */
+
+ public class Dasar
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Halo java");
+   }
+ }
